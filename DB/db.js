278837dae66cbe8e01a3efd4eb67cec14dffceff
putraarga2245-droid <putraarga2245@gmail.com>
2026-09-26 01:14:55 +0700

    Enhance MongoDB connection handling
    Add connection state check and improve error logging.

--- DB/db.js
+++ DB/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectToMongoDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.log(error);
+    console.log("Koneksi Error:", error);
   }
 };
 
